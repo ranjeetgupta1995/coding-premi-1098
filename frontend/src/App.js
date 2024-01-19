c105fc80d10393267c8ba34adc5bfab804688793
Richa Singh <121396620+Richasi@users.noreply.github.com>
2024-01-20 00:37:36 +0530

    blog

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,17 @@
-
-import './App.css';
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
+import AllRoutes from './components/AllRoutes';
 
 function App() {
   return (
-    <div className="App">
-    <h1>This is Main Page.</h1>
-    </div>
+   
+      <div className="App">
+        {/* You can add a header or other common components here */}
+        <AllRoutes />
+        {/* You can add a footer or other common components here */}
+      </div>
+
   );
 }
 
