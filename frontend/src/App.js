c3c72573a0baf2acff02828b9a1f6071ab9bfd37
unknown <anshuldhakate11@gmail.com>
2024-01-20 16:35:03 +0530

    done footer

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,6 +3,7 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import AllRoutes from './components/AllRoutes';
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         {/* You can add a header or other common components here */}
         <Navbar/>
         <AllRoutes />
+        <Footer/>
         {/* You can add a footer or other common components here */}
       </div>
 
