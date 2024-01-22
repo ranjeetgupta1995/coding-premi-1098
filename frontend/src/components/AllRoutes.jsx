6297a3f61383e259b88c7bc2469b9057d8c04f2e
unknown <anshuldhakate11@gmail.com>
2024-01-22 11:35:45 +0530

    done

--- frontend/src/components/AllRoutes.jsx
+++ frontend/src/components/AllRoutes.jsx
@@ -6,10 +6,12 @@ import { Blog } from "./Blogs";
 import About from "./About";
 import Course from "./course/Course";
 import { ContactPage } from "../Pages/ContactPage";
+import { HomePage } from "../Pages/HomePage";
 
 function AllRoutes() {
   return (
     <Routes>
+      <Route path="/" element={<HomePage />} />
       <Route path="/about" element={<About />} />
       <Route path="/blogs" element={<Blog />} />
       <Route path="/course" element={<Course />} />
